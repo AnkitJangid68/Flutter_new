33582adfd7d5a23e8d03b478b269d5154c79fb52
Ankit <ankit.apple.aus@gmail.com>
2023-06-30 16:14:25 +0530

    question2

---
+++ Assignment/q2.dart
@@ -0,0 +1,20 @@
+void main() {
+  int a = 10;
+  int b = 5;
+  var c;
+
+  c = a * b;
+  print('Multiplication is $c');
+
+  c = a / b;
+
+  print('Division is $c');
+
+  c = a + b;
+
+  print('Addition is $c');
+
+  c = a - b;
+
+  print('Subtraction is $c');
+}
