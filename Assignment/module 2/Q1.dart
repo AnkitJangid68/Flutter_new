392f87606cae1d501f067312b3bb1a98333f11d8
Ankit <ankit.apple.aus@gmail.com>
2023-07-17 19:05:54 +0530

    ok

---
+++ Assignment/module 2/Q1.dart
@@ -0,0 +1,23 @@
+// LEAP YEAR
+
+void main() {
+  int? year = 2023;
+
+  if (isLeapYear(year)) {
+    print('$year is a leap year.');
+  } else {
+    print('$year is not a leap year.');
+  }
+}
+
+bool isLeapYear(int year) {
+  if (year % 4 != 0) {
+    return false;
+  } else if (year % 100 != 0) {
+    return true;
+  } else if (year % 400 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
